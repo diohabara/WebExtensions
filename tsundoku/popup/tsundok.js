3a4eec3254cd88e662343726d22b1e8091625a97
diohabara <diohabara@gmail.com>
2019-06-07 15:24:27 +0900

    add html & css & js

---
+++ tsundoku/popup/tsundok.js
@@ -0,0 +1,79 @@
+/**
+* ボタンクリックをリッスンし、ページ内のコンテンツスクリプトに
+* 適切なメッセージを送る
+*/
+function listenForClicks() {
+    document.addEventListener("click", (e) => {
+
+        /**
+        * 動物の名前を受け取って、対応する画像の URL を取得する
+        */
+        function beastNameToURL(beastName) {
+            switch (beastName) {
+                case "Frog":
+                    return browser.extension.getURL("beasts/frog.jpg");
+                case "Snake":
+                    return browser.extension.getURL("beasts/snake.jpg");
+                case "Turtle":
+                    return browser.extension.getURL("beasts/turtle.jpg");
+            }
+        }
+
+        /**
+        * アクティブなタブにページを隠す CSS を挿入して
+        * 動物の URL を取得し、
+        * アクティブなタブのコンテンツスクリプトに "beastify" メッセージを送る 
+        */
+        function beastify(tabs) {
+            browser.tabs.insertCSS({ code: hidePage }).then(() => {
+                let url = beastNameToURL(e.target.textContent);
+                browser.tabs.sendMessage(tabs[0].id, {
+                    command: "beastify",
+                    beastURL: url
+                });
+            });
+        }
+
+
+        /**
+        * ただコンソールにエラーをログ出力する
+        */
+        function reportError(error) {
+            console.error(`Could not beastify: ${error}`);
+        }
+
+        /**
+        * アクティブなタブを取得し、
+        * "beastify()" か "reset()" を適切に呼び出す
+        */
+        if (e.target.classList.contains("beast")) {
+            browser.tabs.query({ active: true, currentWindow: true })
+                .then(beastify)
+                .catch(reportError);
+        }
+        else if (e.target.classList.contains("reset")) {
+            browser.tabs.query({ active: true, currentWindow: true })
+                .then(reset)
+                .catch(reportError);
+        }
+    });
+}
+
+/**
+* スクリプトにエラーがあった。
+* ポップアップのエラーメッセージを表示し、通常の UI を隠す。
+*/
+function reportExecuteScriptError(error) {
+    document.querySelector("#popup-content").classList.add("hidden");
+    document.querySelector("#error-content").classList.remove("hidden");
+    console.error(`Failed to execute beastify content script: ${error.message}`);
+}
+
+/**
+* ポップアップを読み込んだ時、コンテンツスクリプトをアクティブなタブに挿入し、
+* クリックハンドラーを追加する。
+* スクリプトの挿入ができない場合、エラー処理をする。
+*/
+browser.tabs.executeScript({ file: "/content_scripts/beastify.js" })
+    .then(listenForClicks)
+    .catch(reportExecuteScriptError);
